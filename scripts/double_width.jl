b4d473efb5641f02c1726408b866990ca31041d0
smalerba <s.driante@gmail.com>
2020-04-20 09:09:17 +0000

    Noise distribution script

--- scripts/double_width.jl
+++ scripts/double_width.jl
@@ -8,8 +8,6 @@ function vary_σ1(W::Array{Float64,2},σVec::Array{Float64},σ2::Float64,η::Flo
     ε = zeros(length(σVec))
     for (i,σ1) = enumerate(σVec)
         σp = [σ1,σ2]; np = Network(N,Lp,σp,rxrnorm=1,c=c);np.W = W
-        U,V = compute_tuning_curves(np,x_test);
-        println(mean(var(V,dims=2)))
         ε_series = MSE_net_gon(np, η, MC=1)
         ε[i] = mean(ε_series[end-40,end])
     end
@@ -22,7 +20,7 @@ end
 function double_width(N::Int64,σVec::Array{Float64},η::Float64,cVec::Array{Float64},σ2::Float64;nets=4)
     ε = zeros(length(σVec),length(cVec),nets)
     Threads.@threads for net=1:nets
-        #W = randn(N,L)*sqrt(1/L);
+        println("On threads $(Threads.threadid())")
         W = randn(N,Lp[1])*sqrt(1/L); W = hcat(W,W)
         for (i,c)=enumerate(cVec)
             @time ε[:,i,net] = vary_σ1(W,σVec,σ2,η,c)
@@ -33,7 +31,7 @@ end
 L,N = 500,25; η = 0.5;Lp = [250,250];
 cVec =[0.,0.1,0.5,1.,1.5,2.,5.,10.];     σVec = collect((1:2:40)/L);
 σ2Vec = [3/L,5/L,7/L,9/L]
-ε = [double_width(N,σVec,η,cVec,σ2,nets=1) for σ2 = σ2Vec]
+ε = [double_width(N,σVec,η,cVec,σ2) for σ2 = σ2Vec]
 σ2min,σ2max = first(σ2Vec),last(σ2Vec);
 name = savename("double_width" , (@dict N η σ2min σ2max),"jld")
 data = Dict("σVec" => σVec,"σ2Vec" => σ2Vec,"cVec" =>cVec,"ε" => ε,)
