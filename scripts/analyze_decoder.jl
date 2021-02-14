6969c1a242c7220baee38bd4e526a9b978432e5b
smalerba <s.driante@gmail.com>
2021-02-14 18:23:47 +0000

    Running on server -- different N

--- scripts/analyze_decoder.jl
+++ scripts/analyze_decoder.jl
@@ -11,7 +11,7 @@ function Mcomparison(data,MVec::AbstractArray;nepochs=100,ηl=1e-3)
     for M = MVec
         @info "M = $M"
         d = Dict()
-        #my_dec = Chain(Dense(N,M,sigmoid),Dense(M,1,sigmoid))
+        #my_dec = Chain(Dense(N,M,sigmoid),Dense(M,1,linear))
         d[:dec],d[:hist] = train_mlp_decoder((data_trn,data_tst),epochs = nepochs,M=M,η =ηl);
         push!(mlp_decoders,d)
     end
@@ -29,7 +29,7 @@ function analyze_decoder(η::AbstractFloat,W::AbstractArray,σVec::AbstractArray
         decoders = Dict()
         data_trn,data_tst = iidgaussian_dataset(n,η,onehot=true);
         decoders[:ideal_decoder] = ideal_decoder_iidgaussian(n,η,x_test)
-        ~,decoders[:mse_id] = ideal_loss(n,η,x_test,data_tst);
+         ~,decoders[:mse_id] = ideal_loss(n,η,x_test,data_tst);
         decoders[:prob_decoder],decoders[:history_prob] = train_prob_decoder((data_trn,data_tst),epochs=100);
         decoders[:mlp_decoders] = Mcomparison((data_trn,data_tst),MVec)
         dvsσ[i] = decoders
@@ -37,12 +37,18 @@ function analyze_decoder(η::AbstractFloat,W::AbstractArray,σVec::AbstractArray
     return dvsσ
 end
 
-N,L =30,500;
+N,L =35,500;
+NVec = 30:5:80
 η = 0.5
-σVec = (1:3:45)/L;
-MVec = Int.(round.(10 .^range(log10(2),log10(500),length=18)))
-W = sqrt(1/L)*randn(N,L);
-dvsσ = analyze_decoder(η,W,σVec,MVec)
-name = savename("relu_decoder" , (@dict N  L η),"jld2")
+σVec = (5:3:50)/L;
+MVec = Int.(round.(10 .^range(log10(2),log10(500),length=15)))[3:12]
+dvsσVec = []
+for N=NVec
+    W = sqrt(1/L)*randn(N,L);
+    dvsσ = analyze_decoder(η,W,σVec,MVec)
+    push!(dvsσVec,dvsσ)
+end
+Nmin,Nmax = first(Nmin,Nmax)
+name = savename("relu_lin_decoder" , (@dict Nmin Nmax  L η),"jld2")
 data = Dict("σVec" => σVec ,"MVec" => MVec, "W" => W,"dvsσ" => dvsσ)
 safesave(datadir("sims/iidnoise/MLPdec",name) ,data)
